69ea576c3612b16855ae416dea53617b8966851e
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-03-09 15:03:40 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+vector<ll> arr;
+
+void solve() {
+    string s;
+    cin>>s;
+    cout<<s+"??!";
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    //cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
