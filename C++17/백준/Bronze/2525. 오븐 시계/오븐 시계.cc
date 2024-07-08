5dfef0739fb71ee9c40ce94d72d12f55ea10a567
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-07-08 14:10:20 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+vector<ll> arr;
+
+void solve() {
+    int a,b,c;
+    cin >>a>>b>>c;
+    b+=c;
+    int k;
+    k=b/60;
+    b%=60;
+    a+=k;
+    a%=24;
+    cout<<a<<' '<<b;
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    //cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
