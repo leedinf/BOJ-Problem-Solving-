48d41a4af7c346393fc3b3415f994d148231c2b7
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-07-09 21:50:34 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+int a,b;
+vector<ll> arr;
+
+void solve() {
+    while(cin>>a>>b){
+    cout<<a+b<<'\n';
+    }
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
