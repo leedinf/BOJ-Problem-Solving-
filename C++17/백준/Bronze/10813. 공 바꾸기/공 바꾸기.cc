ef5befe93da64f2842e01cdee8b545beab4be5ed
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-07-09 22:19:29 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+int a,b,c;
+vector<ll> arr;
+
+void solve() {
+
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        arr.push_back(i+1);
+    }
+    for(int i=0;i<m;i++){
+        cin>>a>>b;
+        swap(arr[a-1],arr[b-1]);
+    }
+    for(auto k: arr){
+        cout<<k<<' ';
+    }
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
