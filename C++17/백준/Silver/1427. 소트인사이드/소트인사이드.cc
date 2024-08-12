d48480462416413598b8845c1dc929f449860237
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-08-12 15:46:50 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+ll a,b,c,x,y,z;
+vector<ll> arr;
+
+void solve() {
+    string s;
+    cin>>s;
+
+    for(int i=0;i<s.size()-1;i++){
+        for(int j=0;j<=s.size()-i-1;j++){
+            if(s[j+1]>s[j]) swap(s[j+1],s[j]);
+        }
+    }
+
+    cout<<s;
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
