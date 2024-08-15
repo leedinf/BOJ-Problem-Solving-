9256780f168a1f0c287335fb4e39e809472aa2de
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-08-15 14:21:54 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+ll a,b,c,x,y,z;
+vector<ll> arr;
+
+void solve() {
+
+    //약수의 개수가 홀수개인 것의 개수
+    //제곱수만 홀수임.
+    cin>>n;
+    ll sum;
+    for(int i=1;i*i<=n;i++){
+        sum++;
+    }
+    cout<<sum;
+
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
