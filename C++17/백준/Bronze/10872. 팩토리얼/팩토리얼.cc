24099169b4727ea1a50374ff3a41b58036c1214b
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-08-16 15:30:36 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+ll a,b,c,x,y,z;
+vector<ll> arr;
+
+void solve() {
+    cin>>n;
+    x=1;
+    for(int i=1;i<=n;i++){
+        x*=i;
+    }
+    cout<<x;
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
