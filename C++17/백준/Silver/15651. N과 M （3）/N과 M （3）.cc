82b6c54153fdf0ec20546a4794602ce09d660a80
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-08-23 18:26:08 +0900

    [Silver III] Title: N과 M (3), Time: 288 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+ll a,b,c,x,y,z;
+vector<ll> arr(8);
+
+void f(int cnt){
+    if(cnt==m){
+        for(int i=0;i<m;i++) cout<<arr[i]<<' ';
+        cout<<'\n';
+        return;
+    }
+    for(int i=0;i<n;i++){
+        arr[cnt]=i+1;
+        f(cnt+1);
+    }
+}
+void solve() {
+    cin>>n>>m;
+    f(0);
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
