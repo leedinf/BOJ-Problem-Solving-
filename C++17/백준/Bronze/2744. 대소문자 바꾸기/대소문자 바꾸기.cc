414e3b42718545cc16075891666909ef1633cfd1
Leedinf <81596009+leedinf@users.noreply.github.com>
2024-08-27 23:50:34 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+ll n, m, cc;
+ll a,b,c,x,y,z;
+vector<ll> arr;
+
+void solve() {
+    string s;
+    cin>>s;
+
+    for(auto k : s){
+        if(k>='a') cout<<char(k+('A'-'a'));
+        else cout<<char(k+('a'-'A'));
+    }
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // cin >> cc;
+    cc=1;
+    for (int i = 0; i < cc; i++) {
+        solve();
+    }
+
+    return 0;
+}
